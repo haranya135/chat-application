6a0532431da5982673b09301166599ad3b0420a9
haranya135 <haranya135@gmail.com>
2024-10-16 22:46:11 +0530

    Add cors policy

--- server/app.js
+++ server/app.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const app = express();
 
+app.use(cors({
+    origin: 'https://chat-application-ynam-lutr43wh0-haranyas-projects.vercel.app', // allow this specific origin
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true // if you're using cookies or sessions
+  }));
+  
+app.options('*', cors({
+    origin: 'https://chat-application-ynam-lutr43wh0-haranyas-projects.vercel.app',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+  }));
+
 app.post('/auth/login', (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', 'https://chat-application-ynam.vercel.app'); // Set specific allowed origin
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); // Set allowed methods
