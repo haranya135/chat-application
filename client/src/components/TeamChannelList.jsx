7ca2215fef30c825f233662ae9c9d8ad377f8054
haranya135 <haranya135@gmail.com>
2024-10-22 12:46:57 +0530

    Final Commit

--- client/src/components/TeamChannelList.jsx
+++ client/src/components/TeamChannelList.jsx
@@ -2,16 +2,8 @@ import React from 'react';
 
 import { AddChannel } from '../assets';
 
-const TeamChannelList = ({ setToggleContainer, children, error = false, loading, type, isCreating, setIsCreating, setCreateType, setIsEditing }) => {
-    if(error) {
-        return type === 'team' ? (
-            <div className="team-channel-list">
-                <p className="team-channel-list__message">
-                    Connection error, please wait a moment and try again.
-                </p>
-            </div>
-        ) : null
-    }
+const TeamChannelList = ({ setToggleContainer, children, loading, type, isCreating, setIsCreating, setCreateType, setIsEditing }) => {
+    
 
     if(loading) {
         return (
